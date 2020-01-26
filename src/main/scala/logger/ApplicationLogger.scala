c3b2b9eb8e2127a899260a279750b5b6a48096f7
Sayan <shayan1337@gmail.com>
2020-01-26 12:48:37 +0530

    Delete error method in logger trait

--- src/main/scala/logger/ApplicationLogger.scala
+++ src/main/scala/logger/ApplicationLogger.scala
@@ -2,6 +2,4 @@ package logger
 
 class ApplicationLogger extends Logger {
   override def log(info: String): Unit = println("INFO : " + info)
-
-  override def error(error: String): Unit = println("ERROR : " + error)
 }
--- src/main/scala/logger/Logger.scala
+++ src/main/scala/logger/Logger.scala
@@ -2,5 +2,4 @@ package logger
 
 trait Logger {
   def log(info: String): Unit
-  def error(error: String): Unit
 }
