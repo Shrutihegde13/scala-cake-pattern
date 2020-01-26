5f86b840200ab280bc64a6285345dec9dd602e5b
Sayan <shayan1337@gmail.com>
2020-01-26 13:07:53 +0530

    Wrap login service inside a trait

--- src/main/scala/service/login/LoginService.scala
+++ src/main/scala/service/login/LoginService.scala
@@ -3,13 +3,19 @@ package service.login
 import authentication.AzureAuthenticationService
 import logger.ApplicationLogger
 
-class LoginService {
+
+trait LoginService {
 
   this : AzureAuthenticationService with ApplicationLogger =>
 
-  def login(userInfo : String): Boolean = {
-    applicationLoggerImpl.log(userInfo)
-    azureAuthenticationServiceImpl.authenticate(userInfo)
+  val loginServiceImpl : LoginServiceImpl
+
+  class LoginServiceImpl {
+
+    def login(userInfo : String): Boolean = {
+      applicationLoggerImpl.log(userInfo)
+      azureAuthenticationServiceImpl.authenticate(userInfo)
+    }
+
   }
-  
 }
